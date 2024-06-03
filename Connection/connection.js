ac8136a1f154e62d37561dd3824e114ce0a418a5
Avinash Ghadge <144892125+Usernamecopieded@users.noreply.github.com>
2024-06-03 21:40:09 +0530

    Update connection.js

--- Connection/connection.js
+++ Connection/connection.js
@@ -1,1 +1,16 @@
 
+require('dotenv').config()
+const mongoose = require("mongoose");
+// const url ="mongodb+srv://Avinash-Ghadge"
+
+//Connect To DATABASE
+async function dbConnect() {
+  try {
+    await mongoose.connect(process.env.MONGO_URL); //,{dbName:"<collectionName>"}
+    console.log("db Connected");
+  } catch (error) {
+    console.log("error", error);
+  }
+}
+
+module.exports = dbConnect;
