d9f01f9cc242609c1383be64e73332620e092734
Avinash Ghadge <144892125+Usernamecopieded@users.noreply.github.com>
2024-06-03 21:41:47 +0530

    Update Subscribers.js

--- models/Subscribers.js
+++ models/Subscribers.js
@@ -1,1 +1,21 @@
 
+const mongoose = require("mongoose");
+
+//Define Schema here
+const susbcriberSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  subscribedChannel: {
+    type: String,
+    required: true,
+  },
+  subscribedDate: {
+    type: Date,
+    required: false,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("susbcriberSchema", susbcriberSchema);
